590cc37aca0bbb038f490680749df042568c975a
Anton <stroibazaanton@mail.ru>
2022-11-14 21:45:39 +0300

    готовое решение задачи 1

--- DZ1_z1/Program.cs
+++ DZ1_z1/Program.cs
@@ -3,3 +3,23 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Привет! Введите число а и число b, чтбы мы нашли какое из них меньше, а какое больше");
+Console.WriteLine();
+
+Console.WriteLine(" Введите число а");
+string str = Console.ReadLine();
+int a = int.Parse(str);
+
+Console.WriteLine(" Введите число b");
+string str1 = Console.ReadLine();
+int b = int.Parse(str1);
+
+if( a > b)
+{
+    Console.WriteLine(" Max это a, Min это b");
+}
+else 
+{
+    Console.WriteLine( " Max это b, Min это a");
+}
