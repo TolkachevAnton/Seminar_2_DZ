d720271514b3caa8c7c158eef05b1b1b7744ed45
Anton <stroibazaanton@mail.ru>
2022-11-16 17:23:33 +0300

    Готовое решение задачи 1

---
+++ DZ2_z1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+string str1 = Console.ReadLine();
+int num1 = int.Parse(str1);
+
+
+
+Console.WriteLine("Случайное число: " + num1);
+int first = num1 % 100; 
+int second = first / 10; 
+
+Console.WriteLine("Вторая цифра числа: " + second);
