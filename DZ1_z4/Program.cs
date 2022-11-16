536742d7180a00b6ec93a729df0a0961162a2f5f
Anton <stroibazaanton@mail.ru>
2022-11-16 14:59:44 +0300

    Готовое решение задачи 4

--- DZ1_z4/Program.cs
+++ DZ1_z4/Program.cs
@@ -2,3 +2,21 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+string str = Console.ReadLine();
+Console.WriteLine();
+int N = int.Parse(str);
+
+int Na = 2;
+
+if(Na>1)
+{
+    while(Na<=N)
+{
+    Console.WriteLine(Na + " ");
+    Na=Na+2;
+}
+
+}
+
