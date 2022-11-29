fa3a612beba8a90ff12aace91be2d695b7c4bc58
Anton <stroibazaanton@mail.ru>
2022-11-29 18:51:09 +0300

    gotovoe resenie z2

---
+++ DZ4_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+//  и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+int sum = 0;
+
+// for (int i = 0; i < str.Length; i++)
+// {
+//     char currentChar = str[i];
+//     int positionOfChar = Convert.ToInt32(currentChar);
+//     int numberFromChar = int.Parse(currentChar.ToString());
+//     Console.Write($"Сивол: {currentChar}, позиция: {positionOfChar}, число: {numberFromChar} \n");
+// }
+
+for (int i = 0; i < str.Length; i++)
+{
+    int currentNumber = int.Parse(str[i].ToString());
+    sum += currentNumber; // sum = sum + currentNumber
+}
+Console.WriteLine(sum);
