0a0954bded7280f9482e01f3950ad5b6d0a240df
Anton <stroibazaanton@mail.ru>
2022-12-11 16:52:07 +0300

    Gotovoe 8_3

---
+++ DZ8_3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 58: Задайте два двумерных массива (от 0 до 10). 
+// Напишите программу, которая будет находить произведение двух массивов (поэлементное).
+// Например, даны 2 массива:
+// 2 4
+// 3 2
+
+// 3 4
+// 3 3
+// Результирующая матрица будет:
+// 6 16
+// 9 6
+
+int [,] firstMatrix = new int[4,4];
+int [,] secondMatrix = new int [4,4];
+int [,] resultMatrix = new int [4,4];
+
+for (int i = 0; i < 4; i++)
+{
+   Console.WriteLine();
+   for (int j = 0; j < 4; j++)
+   {
+      firstMatrix[i,j] = new Random().Next(1,11);
+      
+   
+      Console.Write(firstMatrix[i,j] + "\t");
+      
+   }
+  
+}
+System.Console.WriteLine();
+
+for (int i = 0; i < 4; i++)
+{
+   Console.WriteLine();
+   for (int j = 0; j < 4; j++)
+   {
+      secondMatrix[i,j] = new Random().Next(1,11);
+
+      Console.Write(secondMatrix[i,j] + "\t");
+         
+   }
+  
+}
+System.Console.WriteLine();
+
+for (int i = 0; i < 4; i++)
+{
+
+   Console.WriteLine();
+   for (int j = 0; j < 4; j++)
+   {
+      resultMatrix[i,j] = firstMatrix[i,j]* secondMatrix[i,j];
+   
+      Console.Write(resultMatrix[i,j] + "\t");
+      
+   }
+  
+}
